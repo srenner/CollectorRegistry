0d450743fd272d0fcade06d6ddd416a130d41c4d
Shawn Renner <shawn.renner@gmail.com>
2022-11-30 13:50:28 -0600

    update

---
+++ CollectorRegistry/Shared/Models/Entry.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectorRegistry.Shared.Models
+{
+    public class Entry
+    {
+        public int EntryID { get; set; }
+
+        public int SiteID { get; set; }
+        public Site Site { get; set; }
+
+        [DefaultValue(true)]
+        public bool OnRoad { get; set; }
+        
+        [DefaultValue(false)]
+        public bool Deceased { get; set; }
+    }
+}
--- CollectorRegistry/Shared/Models/Site.cs
+++ CollectorRegistry/Shared/Models/Site.cs
@@ -11,8 +11,9 @@ namespace CollectorRegistry.Shared.Models
         public int SiteID { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public string AboutText { get; set; }
         public string Subdomain { get; set; }
         public string Logo { get; set; }
-
+        public string? VinRegex { get; set; }
     }
 }
