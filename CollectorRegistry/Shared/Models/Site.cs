1cca1152a36c150b2ef7dc9d994b5a4787e605f8
Shawn Renner <shawn.renner@gmail.com>
2022-11-30 09:39:15 -0600

    update

---
+++ CollectorRegistry/Shared/Models/Site.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectorRegistry.Shared.Models
+{
+    public class Site
+    {
+        public int SiteID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Subdomain { get; set; }
+        public string Logo { get; set; }
+
+    }
+}
