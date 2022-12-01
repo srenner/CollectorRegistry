f12b62223f1216ca4aee1163032990f5269293a6
Shawn Renner <shawn.renner@gmail.com>
2022-12-01 09:34:16 -0600

    modeling

--- CollectorRegistry/Shared/Models/Entry.cs
+++ CollectorRegistry/Shared/Models/Entry.cs
@@ -12,6 +12,7 @@ namespace CollectorRegistry.Shared.Models
         public int EntryID { get; set; }
 
         public int SiteID { get; set; }
+        
         public Site Site { get; set; }
 
         [DefaultValue(true)]
@@ -19,5 +20,35 @@ namespace CollectorRegistry.Shared.Models
         
         [DefaultValue(false)]
         public bool Deceased { get; set; }
+        
+        [DefaultValue(false)]
+        public bool ForSale { get; set; }
+
+        public decimal? ListPrice { get; set; }
+
+        public decimal? TransactionPrice { get; set; }
+
+
+
+
+
+        public string? Owner { get; set; }
+        public string? City { get; set; }
+        public string? Region { get; set; }
+        public string? PostalCode { get; set; }
+        public string? Country { get; set; }
+
+        public int? Mileage { get; set; }
+        public string? Comments { get; set; }
+
+
+        public DateTime EntryDateTime { get; set; }
+        public DateTime ActualEntryDateTime { get; set; }
+
+
+        [DefaultValue(false)]
+        public bool IsDeleted { get; set; }
+
+
     }
 }
