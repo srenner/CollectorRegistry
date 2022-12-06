691e9a1ff369963abacc561480d95835ff92b5e1
Shawn Renner <shawn.renner@gmail.com>
2022-12-06 15:50:43 -0600

    update viewmodel

--- CollectorRegistry/Shared/ViewModels/SiteViewModels.cs
+++ CollectorRegistry/Shared/ViewModels/SiteViewModels.cs
@@ -10,10 +10,17 @@ namespace CollectorRegistry.Shared.ViewModels
     {
         public int SiteID { get; set; }
         public string Title { get; set; }
-        public string Description { get; set; }
-        public string AboutText { get; set; }
         public string Subdomain { get; set; }
-        public string Logo { get; set; }
-        public string? VinRegex { get; set; }
+        public string? Description { get; set; }
+        public string? AboutText { get; set; }
+        public string? Logo { get; set; }
+        public string? SerialNumberRegex { get; set; }
+        public DateTime CreationDate { get; set; }
+        public bool IsApproved { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDeleted { get; set; }
+        
+        //item viewmodel collection
+        //entry definition viewmodel collection
     }
 }
