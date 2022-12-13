83d820fb71ef6a5a6220f5325aa0820901b462e0
Shawn Renner <shawn.renner@gmail.com>
2022-12-13 14:35:51 -0600

    add site color cols

---
+++ CollectorRegistry/Server/Migrations/20221213203514_site-colors.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CollectorRegistry.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class sitecolors : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PrimaryColor",
+                table: "Sites",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SecondaryColor",
+                table: "Sites",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PrimaryColor",
+                table: "Sites");
+
+            migrationBuilder.DropColumn(
+                name: "SecondaryColor",
+                table: "Sites");
+        }
+    }
+}
