84927375d38779708bcdc1f66aec092a2131778d
Shawn Renner <shawn.renner@gmail.com>
2022-12-23 21:18:42 -0600

    rabbit mq

---
+++ CollectorRegistry/Shared/EntryAddress.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectorRegistry.Shared
+{
+    [Serializable]
+    public class EntryAddress
+    {
+        public int EntryID { get; set; }
+        public string? City { get; set; }
+        public string? Region { get; set; }
+        public string? PostalCode { get; set; }
+        public string? Country { get; set; }
+
+        public decimal? GeoLat { get; set; }
+        public decimal? GeoLong { get; set; }
+    }
+}
