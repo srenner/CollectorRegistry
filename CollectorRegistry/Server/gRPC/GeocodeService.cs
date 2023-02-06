3abd58bf7b42f7e904425575e715b70b13e2ed4e
Shawn Renner <shawn.renner@gmail.com>
2023-02-05 22:03:42 -0600

    start geocode grpc service

---
+++ CollectorRegistry/Server/gRPC/GeocodeService.cs
@@ -0,0 +1,23 @@
+﻿using CollectorRegistry.Shared.Protos;
+using Grpc.Core;
+
+namespace CollectorRegistry.Server.gRPC
+{
+    public class GeocodeService : CollectorRegistry.Shared.Protos.Geocode.GeocodeBase
+    {
+        private readonly ILogger<GeocodeService> _logger;
+
+        public GeocodeService(ILogger<GeocodeService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<GeocodeResponse> UpdateEntry(GeocodeUpdateRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new GeocodeResponse
+            {
+                Message = "Hello " + request.GeoDescr
+            });
+        }
+    }
+}
