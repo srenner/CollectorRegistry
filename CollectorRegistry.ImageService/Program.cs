bf29afd3dcf4b9ca806792574fd2becf84b94c51
Shawn Renner <shawn.renner@gmail.com>
2023-02-09 22:20:29 -0600

    add blank image service project

---
+++ CollectorRegistry.ImageService/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CollectorRegistry.ImageService
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
+        }
+    }
+}
