facf2c1eddb1b4907c93c17c4b83c4a92d85af2c
Shawn Renner <shawn.renner@gmail.com>
2023-02-10 21:18:13 -0600

    ImageService as a service

---
+++ CollectorRegistry.ImageService/ImageService.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CollectorRegistry.ImageService
+{
+    internal class ImageService : IHostedService
+    {
+        private readonly ILogger<ImageService> _logger;
+        private readonly IHostApplicationLifetime _appLifetime;
+        //private readonly IOptions<ConnectionSettings> _settings;
+
+        private IModel _channel;
+        private IConnection _connection;
+        private EventingBasicConsumer _consumer;
+
+        public ImageService(ILogger<ImageService> logger, IHostApplicationLifetime appLifetime)
+        {
+            _logger = logger;
+            _appLifetime = appLifetime;
+
+
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _appLifetime.ApplicationStopping.Register(OnStopping);
+            _appLifetime.ApplicationStopped.Register(OnStopped);
+            _appLifetime.ApplicationStarted.Register(() =>
+            {
+                Task.Run(async () =>
+                {
+                    try
+                    {
+
+                        AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+                        
+
+
+                        while (!cancellationToken.IsCancellationRequested)
+                        {
+                            Task.Delay(-1);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.LogError(ex, "Unhandled exception: " + ex.Message);
+                    }
+                    finally
+                    {
+                        _logger.LogDebug("Stopping ImageService");
+                        _appLifetime.StopApplication();
+                    }
+                });
+            });
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+
+        private void OnStopping()
+        {
+            _connection.Close();
+        }
+
+        private void OnStopped()
+        {
+            // ...
+        }
+
+
+
+    }
+}
--- CollectorRegistry.ImageService/Program.cs
+++ CollectorRegistry.ImageService/Program.cs
@@ -1,11 +1,30 @@
-﻿namespace CollectorRegistry.ImageService
+﻿using Microsoft.Extensions.Hosting;
+using CollectorRegistry.Shared.Protos;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.NetworkInformation;
+using System.Text;
+
+namespace CollectorRegistry.ImageService
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.ReadKey();
+            Console.WriteLine("ImageService starting at " + DateTime.Now.ToLongTimeString());
+
+            await Host.CreateDefaultBuilder(args)
+                 .ConfigureServices((hostContext, services) =>
+                 {
+                     var builder = new ConfigurationBuilder().AddJsonFile($"appsettings.json", true, true);
+                     var config = builder.Build();
+                     //services.Configure<ConnectionSettings>(config.GetSection("ConnectionSettings"));
+                     services.AddHostedService<ImageService>();
+                     services.AddOptions();
+                 })
+                 .RunConsoleAsync();
+
+            Console.WriteLine("Shutting down at " + DateTime.Now.ToLongTimeString());
         }
     }
 }
