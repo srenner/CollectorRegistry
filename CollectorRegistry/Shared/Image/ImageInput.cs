b0aae9269c418481046088a795a271efa2a8c137
Shawn Renner <shawn.renner@gmail.com>
2023-04-03 21:29:50 -0500

    update ImageInput fields

--- CollectorRegistry/Shared/Image/ImageInput.cs
+++ CollectorRegistry/Shared/Image/ImageInput.cs
@@ -8,10 +8,10 @@ namespace CollectorRegistry.Shared.Image
 {
     public record struct ImageInput
     {
+        public int EntryID { get; set; }
 
-
-        public string FullPath { get; set; }
-        public string Watermark { get; set; }
+        public string UploadPath { get; set; }
+        public string WatermarkText { get; set; }
 
 
     }
